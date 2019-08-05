9d06bba6f65466cc8aab507a10fbf6e6e78e7c9a
vackosar <admin@vaclavkosar.com>
2019-08-05 20:00:28 +0200

    back anki to syncthing

--- home/bin/backup-anki.sh
+++ home/bin/backup-anki.sh
@@ -2,4 +2,4 @@
 set -uxe
 
 ankiBackupsDir="/home/vackosar/.local/share/Anki2/User 1/backups/";
-cp "$ankiBackupsDir/$(ls -1t "$ankiBackupsDir"|sed 1q)" "/home/vackosar/dropbox/bkp/ankiBackup.colpkg";
+cp "$ankiBackupsDir/$(ls -1t "$ankiBackupsDir"|sed 1q)" "/home/vackosar/bkpSync/ankiBackup.colpkg";
