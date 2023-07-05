8e340678588d437950831d49d5dfb6780b20db70
Vaclav Kosar <admin@vaclavkosar.com>
2023-07-05 21:48:10 +0200

    duck

--- home/bin/i3-status-custom.sh
+++ home/bin/i3-status-custom.sh
@@ -1,1 +1,1 @@
-i3status | { while read status; do echo "Take action to learn. Don't waste time autopiloting! | $status"; done; }
+i3status | { while read status; do echo "Take action to learn. Don't waste time autopiloting! 🦆 | $status"; done; }
