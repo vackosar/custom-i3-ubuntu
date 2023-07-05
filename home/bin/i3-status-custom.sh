0f519eba6fbc84b114c12d043455b99e53283473
Vaclav Kosar <admin@vaclavkosar.com>
2023-07-05 16:27:19 +0200

    status dont waste time

--- home/bin/i3-status-custom.sh
+++ home/bin/i3-status-custom.sh
@@ -1,1 +1,1 @@
-i3status | { while read status; do echo "Don't waste time! | $status"; done; }
+i3status | { while read status; do echo "Take action to learn. Don't waste time autopiloting! | $status"; done; }
